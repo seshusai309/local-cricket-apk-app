a29486408d2cd497181e7298541bd91951554fc7
Sai Sesha Reddy <saiseshar@gmail.com>
2026-02-17 17:02:31 +0530

    first commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Sai Sesha Reddy</Text>
       <StatusBar style="auto" />
     </View>
   );
